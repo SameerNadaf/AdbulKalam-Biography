ceefc7a6fd55086bfedc1840312fd948bf1a6e37
Sameer Nadaf <nsameernadaf@gmail.com>
2025-09-04 16:46:07 +0530

    Initial Commit

---
+++ AbdulKalam/AbdulKalamApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbdulKalamApp.swift
+//  AbdulKalam
+//
+//  Created by Sameer  on 04/09/25.
+//
+
+import SwiftUI
+
+@main
+struct AbdulKalamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
